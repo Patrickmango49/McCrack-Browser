0f42f0b4edf644262487183ceeadf102bb2bcb28
Patrick_Mango <patrickmango499@outlook.com>
2026-04-10 07:40:38 -0400

    Integrate Ultraviolet proxy scripts and config

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,12 +1,11 @@
-// This file overwrites the stock UV config.js
-
 self.__uv$config = {
-	prefix: "/uv/service/",
-	encodeUrl: Ultraviolet.codec.xor.encode,
-	decodeUrl: Ultraviolet.codec.xor.decode,
-	handler: "/uv/uv.handler.js",
-	client: "/uv/uv.client.js",
-	bundle: "/uv/uv.bundle.js",
-	config: "/uv/uv.config.js",
-	sw: "/uv/uv.sw.js",
+	prefix: "/sw/",
+	bare: "https://uv-on-vercel-two-pi.vercel.app/bare/",
+	encodeUrl: Ultraviolet.encodeUrl,
+	decodeUrl: Ultraviolet.decodeUrl,
+	handler: "https://uv-on-vercel-two-pi.vercel.app/uv/uv.handler.js",
+	client: "https://uv-on-vercel-two-pi.vercel.app/uv/uv.client.js",
+	bundle: "https://uv-on-vercel-two-pi.vercel.app/uv/uv.bundle.js",
+	config: "https://uv-on-vercel-two-pi.vercel.app/uv/uv.config.js",
+	sw: "https://uv-on-vercel-two-pi.vercel.app/uv/sw.js",
 };
